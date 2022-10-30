e88e39e9f4d1fb977d9cfd6ea66fb27d6bff776e
Alexandr Tesla <aptity@gmail.com>
2022-10-30 22:31:21 +0300

    refresh

---
+++ Dz/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Дополнительно.
+// Напишите программу, которая принимает на вход число (N) и выдает таблицу простых чисел от 1 до N.
+
+
+void SimpleDigit(int digit)
+{
+    for (int i = 2; i < digit - 1; i++)
+    {
+        for (int j = 2; j < digit - 1; j++)
+        {
+            if (i % j != 0) Console.WriteLine($"{i}");
+        }
+    }
+}
+
+Console.WriteLine("Введите число");
+int digit = int.Parse(Console.ReadLine()!);
+Console.ReadLine();
+SimpleDigit(digit);
