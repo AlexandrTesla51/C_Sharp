55d186b01cc5cacc8abbac3943fcb842eea8fe61
Alexandr Tesla <aptity@gmail.com>
2022-12-25 17:53:16 +0300

    Second stage

--- Dz/Program.cs
+++ Dz/Program.cs
@@ -1,10 +1,30 @@
 ﻿string[] NewArray(string[] array)
 {
+    string[] massive = new string[6];
+    int j = 0;
     for (int i = 0; i < array.Length; i++)
+
+    {
         if (array[i].Length <= 3)
         {
-            
+            massive[j] = array[i];
+            j++;
         }
+    }
+
+    return massive;
 }
 
-string[] array = new string[] { "hello", "23", "Sun", "12", "Ti", "Veles" };
+void PrintArray(string[] massive)
+{
+    for (int i = 0; i < massive.Length; i++)
+    {
+        Console.Write(massive[i]);
+        Console.Write(" ");
+    }
+}
+
+string[] array = { "hello", "23", "Sun", "12", "Ti", "Veles" };
+
+
+PrintArray(NewArray(array));
