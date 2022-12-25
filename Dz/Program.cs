26b94bf471f4e92690d27ac2aee1e21e054cb8c8
Alexandr Tesla <aptity@gmail.com>
2022-12-25 16:01:14 +0300

    + Itogovay rabota
    first stage

---
+++ Dz/Program.cs
@@ -0,0 +1,10 @@
+﻿string[] NewArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        if (array[i].Length <= 3)
+        {
+            
+        }
+}
+
+string[] array = new string[] { "hello", "23", "Sun", "12", "Ti", "Veles" };
